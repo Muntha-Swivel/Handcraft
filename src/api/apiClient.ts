91a43a1d2c7ad3b86a22011051a20f070471005f
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-10 19:00:09 +0530

    changed base url

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 const axiosClient = axios.create({
   withCredentials: true,
   baseURL:
-    "http://employeemanager-env.eba-mhwcuic3.ap-northeast-1.elasticbeanstalk.com/",
+    "http://employeemanager-env.eba-mhwcuic3.ap-northeast-1.elasticbeanstalk.com",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
