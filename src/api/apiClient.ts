42bbd66b9f5219938ac217c0c495eed57e3e28da
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-10 19:33:09 +0530

    testing

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  withCredentials: true,
   baseURL:
     "http://employeemanager-env.eba-mhwcuic3.ap-northeast-1.elasticbeanstalk.com",
   headers: {
