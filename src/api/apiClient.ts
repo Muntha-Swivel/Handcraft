9c20af04a7a0377bfdbd6a26af600aa8c4c5bf84
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-10 19:22:15 +0530

    changed header

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,12 +2,11 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosClient = axios.create({
-  withCredentials: true,
-  baseURL:
-    "http://employeemanager-env.eba-mhwcuic3.ap-northeast-1.elasticbeanstalk.com",
+  baseURL: "http://localhost:8080",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
+    "Access-Control-Allow-Credentials": true,
   },
 });
 
