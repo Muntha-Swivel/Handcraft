dacc5f4e3016e9459a04649d5fd2c5b935789737
Muntha-Swivel <munthasir@swivelgroup.com.au>
2023-02-15 12:11:12 +0530

    changed api

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   withCredentials: true,
-  baseURL:
-    "http://employeemanager-env.eba-mhwcuic3.ap-northeast-1.elasticbeanstalk.com/",
+  baseURL: "https://jwelcraft.herokuapp.com",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
